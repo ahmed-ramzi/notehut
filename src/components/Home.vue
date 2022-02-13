facf7470668e6bc9dadd52d5e45bcfe00ea26806
Ramzi <ramzi@dataddo.com>
2022-02-13 18:57:36 +0100

    Alignment and style fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,45 +3,59 @@
     <section class="p-4 flex justify-between items-center">
         <h1>Notes</h1>
         <button
-            class="border-2 w-36 h-9 rounded-lg border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white active:bg-rose-400 uppercase"
+            class="border-2 w-36 h-9 rounded-lg btn-create-note border-rose-500 text-rose-500 uppercase"
         >
             <span class="hidden lg:block"> + New Note </span>
             <span class="lg:hidden"> + Create New </span>
         </button>
     </section>
 
-    <section class="p-2 flex">
+    <section :class="!notes ? 'border-b-2' : 'p-2 flex'">
         <!-- Notes Nav -->
         <nav
             :class="
                 !currentNote
                     ? 'w-full'
-                    : 'min-w-[35%] w-auto bg-gray-100 rounded-md'
+                    : 'min-w-[35%] w-auto bg-gray-100  rounded-md shadow-lg'
             "
+            class="mt-1"
         >
             <ul class="grid" v-if="notes.length">
                 <li v-for="note in notes" :key="note.id">
                     <button
                         @click="currentNote = note"
-                        class="w-full text-left pl-2 py-4 rounded-md"
+                        class="w-full text-left pl-2 py-2 rounded-md"
                         :class="
                             note === currentNote
                                 ? 'bg-rose-500 text-white italic'
-                                : 'hover:bg-rose-200'
+                                : 'hover:bg-rose-200 text-gray-500'
                         "
                     >
-                        <h2>
-                            {{ note.title }}
+                        <h2 class="">
+                            {{ note.title || 'New note' }}
                         </h2>
                     </button>
                 </li>
             </ul>
-            <p v-else>No notes saved</p>
+            <p v-else class="text-gray-400 italic p-2">No notes saved</p>
         </nav>
 
-        <div v-if="currentNote" class="w-full p-4">
-            <h2>{{ currentNote.title }}</h2>
-            <p>{{ currentNote.contents }}</p>
+        <div v-if="currentNote" class="w-full px-2 space-y-3">
+            <input
+                type="text"
+                name="title"
+                class="font-extralight text-3xl text-gray-400 w-full p-2 outline-none rounded-md"
+                v-model="currentNote.title"
+                :placeholder="currentNote.title || 'New note'"
+            />
+
+            <textarea
+                name="contents"
+                rows="10"
+                class="w-full rounded-md outline-none px-2 py-2 text-gray-400 font-light"
+                v-model="currentNote.contents"
+                :placeholder="currentNote.contents || 'Jot something here...'"
+            ></textarea>
         </div>
     </section>
 </template>
@@ -64,3 +78,9 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.btn-create-note:hover {
+    @apply text-white bg-rose-500 shadow-xl shadow-rose-200;
+}
+</style>
