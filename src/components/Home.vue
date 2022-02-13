5e0a3bae1ada75a11e3235c1928759ccc1bd3ff4
Ramzi <ramzi@dataddo.com>
2022-02-13 04:18:47 +0100

    CSS stylising

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,35 @@
 <template>
     <!-- Header -->
-    <section class="p-4 flex justify-between">
+    <section class="p-4 flex justify-between items-center">
         <h1>Notes</h1>
         <button
-            class="border-2 w-36 rounded-lg border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white active:bg-rose-400 uppercase"
+            class="border-2 w-36 h-9 rounded-lg border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white active:bg-rose-400 uppercase"
         >
             <span class="hidden lg:block"> + New Note </span>
             <span class="lg:hidden"> + Create New </span>
         </button>
     </section>
 
-    <section class="px-8 py-4 flex">
+    <section class="p-2 flex">
         <!-- Notes Nav -->
         <nav
-            class="border"
-            :class="!currentNote ? 'w-full' : 'min-w-[35%] w-auto'"
+            :class="
+                !currentNote
+                    ? 'w-full'
+                    : 'min-w-[35%] w-auto bg-gray-100 rounded-md'
+            "
         >
-            <ul class="grid gap-4" v-if="notes.length">
+            <ul class="grid" v-if="notes.length">
                 <li v-for="note in notes" :key="note.id">
-                    <button @click="currentNote = note" class="hover:underline">
+                    <button
+                        @click="currentNote = note"
+                        class="w-full text-left pl-2 py-4 rounded-md"
+                        :class="
+                            note === currentNote
+                                ? 'bg-rose-500 text-white italic'
+                                : 'hover:bg-rose-200'
+                        "
+                    >
                         <h2>
                             {{ note.title }}
                         </h2>
@@ -28,7 +39,7 @@
             <p v-else>No notes saved</p>
         </nav>
 
-        <div v-if="currentNote" class="border w-full p-4">
+        <div v-if="currentNote" class="w-full p-4">
             <h2>{{ currentNote.title }}</h2>
             <p>{{ currentNote.contents }}</p>
         </div>
@@ -40,9 +51,9 @@ import { ref } from 'vue'
 export default {
     setup() {
         const notes = ref([
-            { id: '1', title: 'First Notes', contents: 'This is a content' },
-            { id: '2', title: 'Second Notes', contents: 'This is a content' },
-            { id: '3', title: 'Third Notes', contents: 'This is a content' },
+            { id: '1', title: 'First Note', contents: 'This is a content' },
+            { id: '2', title: 'Second Note', contents: 'This is a content' },
+            { id: '3', title: 'Third Note', contents: 'This is a content' },
         ])
         const currentNote = ref(null)
 
