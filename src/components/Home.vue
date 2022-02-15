26696c806104f455ef2189e732a8afe30dab3575
Ramzi <ramzi@dataddo.com>
2022-02-15 23:24:37 +0100

    design update + logic

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,34 +1,54 @@
 <template>
     <!-- Header -->
-    <section class="p-4 flex justify-between items-center">
-        <h1>Notes</h1>
+    <section class="p-4 flex justify-between items-center text-center">
+        <span>
+            <h1 v-if="!currentNote">Notes</h1>
+            <h2 v-else class="btn-back" @click="currentNote = null">Back</h2>
+        </span>
+
         <button
-            class="border-2 w-36 h-9 rounded-lg btn-create-note border-rose-500 text-rose-500 uppercase"
+            class="
+                border-2
+                w-36
+                h-9
+                rounded-lg
+                btn-create-note
+                border-rose-500
+                text-rose-500
+                uppercase
+            "
+            @click="createNote"
         >
-            <span class="hidden lg:block"> + New Note </span>
-            <span class="lg:hidden"> + Create New </span>
+            <!-- <span class="hidden lg:block"> + New Note </span> -->
+            <span> + Create New </span>
         </button>
     </section>
 
-    <section :class="!notes ? 'border-b-2' : 'p-2 flex'">
+    <section class="h-screen flex">
         <!-- Notes Nav -->
         <nav
             :class="
                 !currentNote
                     ? 'w-full'
-                    : 'min-w-[35%] w-auto bg-gray-100  rounded-md shadow-lg'
+                    : 'min-w-[35%] w-auto rounded-r-md border-r shadow-lg hidden md:block'
             "
-            class="mt-1"
+            class="mt-1 mb-2 px-2 h-full"
         >
-            <ul class="grid" v-if="notes.length">
+            <ul
+                :class="
+                    !currentNote
+                        ? 'grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  gap-4  mx-2'
+                        : 'grid gap-4'
+                "
+                v-if="notes.length"
+            >
                 <li v-for="note in notes" :key="note.id">
                     <button
                         @click="currentNote = note"
-                        class="w-full text-left pl-2 py-2 rounded-md"
                         :class="
                             note === currentNote
-                                ? 'bg-rose-500 text-white italic'
-                                : 'hover:bg-rose-200 text-gray-500'
+                                ? 'note-selected bg-rose-500 text-white'
+                                : 'notes-unselected text-gray-500'
                         "
                     >
                         <h2 class="">
@@ -40,19 +60,38 @@
             <p v-else class="text-gray-400 italic p-2">No notes saved</p>
         </nav>
 
-        <div v-if="currentNote" class="w-full px-2 space-y-3">
+        <!-- Note Writing Section -->
+        <div v-if="currentNote" class="w-full space-y-3 mx-2">
             <input
                 type="text"
                 name="title"
-                class="font-extralight text-3xl text-gray-400 w-full p-2 outline-none rounded-md"
+                class="
+                    font-extralight
+                    text-3xl text-gray-400
+                    w-full
+                    p-2
+                    outline-none
+                    rounded-md
+                "
                 v-model="currentNote.title"
                 :placeholder="currentNote.title || 'New note'"
+                ref="noteTitle"
             />
 
             <textarea
                 name="contents"
                 rows="10"
-                class="w-full rounded-md outline-none px-2 py-2 text-gray-400 font-light"
+                class="
+                    w-full
+                    rounded-md
+                    outline-none
+                    px-2
+                    py-2
+                    text-gray-400
+                    font-light
+                    h-[90%]
+                    resize-none
+                "
                 v-model="currentNote.contents"
                 :placeholder="currentNote.contents || 'Jot something here...'"
             ></textarea>
@@ -65,15 +104,27 @@ import { ref } from 'vue'
 export default {
     setup() {
         const notes = ref([
+            // Mock data
             { id: '1', title: 'First Note', contents: 'This is a content' },
             { id: '2', title: 'Second Note', contents: 'This is a content' },
             { id: '3', title: 'Third Note', contents: 'This is a content' },
+            { id: '4', title: 'Third Note', contents: 'This is a content' },
         ])
         const currentNote = ref(null)
 
+        const noteTitle = ref()
+
+        const createNote = () => {
+            const newNote = { title: '', contents: '' }
+            notes.value.push(newNote)
+            currentNote.value = newNote
+            noteTitle.value.focus()
+        }
         return {
             notes,
             currentNote,
+            createNote,
+            noteTitle,
         }
     },
 }
@@ -81,6 +132,38 @@ export default {
 
 <style scoped>
 .btn-create-note:hover {
-    @apply text-white bg-rose-500 shadow-xl shadow-rose-200;
+    @apply text-white bg-rose-500 shadow-md shadow-rose-300;
+}
+.btn-create-note:active {
+    @apply bg-rose-400;
+}
+.btn-back {
+    @apply text-rose-500 border border-white px-6 py-1 rounded-lg;
+}
+.btn-back:hover {
+    @apply border border-rose-500;
+}
+.btn-back:active {
+    @apply bg-rose-400;
+}
+/* .note-grid-mobile {
+    @apply w-full text-left pl-2 py-2 bg-gray-100;
+}
+
+.note-grid:hover{
+    @apply bg-rose-200 shadow-md shadow-rose-300 text-gray-500
+} */
+
+.note-selected,
+.notes-unselected {
+    @apply w-full text-left pl-2 py-2 border shadow-md rounded-lg;
+}
+
+/* .notes-unselected {
+    @apply rounded-lg md:w-44 md:h-16 md:text-center;
+} */
+
+.notes-unselected:hover {
+    @apply bg-rose-200 shadow-md shadow-rose-300;
 }
 </style>
