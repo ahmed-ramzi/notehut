ee5832e22d55e1c88347f46c26bce659e1caccb3
Ramzi <ramzi@dataddo.com>
2022-06-30 16:45:57 +0200

    Testing createWebHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router"
 
 export const routes = [
   {
@@ -29,7 +29,7 @@ export const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
