ff6758d10b30fa5f81e804e4d0ab559319687c7a
Ramzi <ramzi@dataddo.com>
2022-06-30 16:49:17 +0200

    Revert createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 export const routes = [
   {
@@ -29,7 +29,7 @@ export const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
