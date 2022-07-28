d7bed131a783c9408cbd501c928ebb949799a5f3
Ramzi <ramzi@dataddo.com>
2022-07-28 22:56:26 +0200

    Added: Firestore Config file

---
+++ src/firestore.ts
@@ -0,0 +1,28 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: `${import.meta.env.VITE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_AUTH_DOMAIN}`,
+  projectId: "notehut-14bd0",
+  storageBucket: "notehut-14bd0.appspot.com",
+  messagingSenderId: "56061727737",
+  appId: "1:56061727737:web:53acf575ab6f7a5036e328",
+  measurementId: "G-Y3B6RD5SRC",
+}
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+export default db
