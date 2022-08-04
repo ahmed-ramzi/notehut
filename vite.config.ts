c08c5497849ef2c19fcf20bebcaafafe84a9a9f0
Ramzi <ramzi@dataddo.com>
2022-08-04 13:01:24 +0200

    Added: Tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,19 @@ import * as path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  clearScreen: false,
+  server: {
+    port: 8080,
+    strictPort: true,
+  },
+  envPrefix: ["VITE_", "TAURI_"],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  build: {
+    target: ["es2021", "chrome97", "safari13"],
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
   define: {
     "process.env": {},
