1fa6246b56bdfdbc34bd5f899c3e2a5e071b8e40
Ramzi <ramzi@dataddo.com>
2022-12-11 17:38:00 +0100

    Fix delete button appearing onScrolling

--- src/pages/GroupsPage.vue
+++ src/pages/GroupsPage.vue
@@ -1,34 +1,36 @@
 <template>
   <MenuLayout back-btn="HomePage" header-label="groups">
-    <div class="flex flex-col space-y-4">
-      <div
-        v-for="group in groups"
-        :key="group.id"
-        class="rounded-2xl h-24 bg-gradient-to-r p-4 from-slate-100 via-slate-300 to-slate-500 shadow-md hover:motion-safe:shadow-xl cursor-pointer duration-300 flex justify-between items-center hover:motion-safe:ring-2 ring-slate-900"
-        @click="onClick(group.id)"
-      >
-        <div class="flex flex-col">
-          <h3 class="text-slate-900 font-light">{{ group.name }}</h3>
-          <small class="text-slate-500 text-size italic">Created: {{ group.created_at }}</small>
-        </div>
-
-        <div class="h-full space-x-2 flex items-center">
-          <!-- TODO Fix not more than 3 Icons to appear -->
-          <div class="relative h-full">
-            <Avatar
-              v-for="(member, index) in group.members"
-              :key="member.id"
-              :name="member.name"
-              width="w-16"
-              class="absolute"
-              :class="index === 0 ? `right-0` : index === 1 ? 'right-10' : index === 2 ? `right-20` : 'hidden'"
-            />
+    <template #content>
+      <div class="flex flex-col space-y-4">
+        <div
+          v-for="group in groups"
+          :key="group.id"
+          class="rounded-2xl h-24 bg-gradient-to-r p-4 from-slate-100 via-slate-300 to-slate-500 shadow-md hover:motion-safe:shadow-xl cursor-pointer duration-300 flex justify-between items-center hover:motion-safe:ring-2 ring-slate-900"
+          @click="onClick(group.id)"
+        >
+          <div class="flex flex-col">
+            <h3 class="text-slate-900 font-light">{{ group.name }}</h3>
+            <small class="text-slate-500 text-size italic">Created: {{ group.created_at }}</small>
           </div>
 
-          <!-- <Preferences /> -->
+          <div class="h-full space-x-2 flex items-center">
+            <!-- TODO Fix not more than 3 Icons to appear -->
+            <div class="relative h-full">
+              <Avatar
+                v-for="(member, index) in group.members"
+                :key="member.id"
+                :name="member.name"
+                width="w-16"
+                class="absolute"
+                :class="index === 0 ? `right-0` : index === 1 ? 'right-10' : index === 2 ? `right-20` : 'hidden'"
+              />
+            </div>
+
+            <!-- <Preferences /> -->
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </MenuLayout>
 </template>
 
