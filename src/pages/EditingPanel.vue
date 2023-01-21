7f449e1b2ece3f2ca9f3ca489bb90ee265872129
Ramzi <ramzi@dataddo.com>
2023-01-21 03:36:30 +0100

    UI restructure

--- src/pages/EditingPanel.vue
+++ src/pages/EditingPanel.vue
@@ -1,48 +1,71 @@
 <template>
-  <div class="h-screen p-4 space-y-4 flex flex-col">
-    <HeaderSection back-btn="back"></HeaderSection>
-    <input
-      ref="noteTitle"
-      v-model="noteLabel"
-      type="text"
-      name="title"
-      :placeholder="noteLabel || 'New note'"
-      class="font-extralight text-3xl text-slate-500 w-full bg-transparent outline-none rounded-md"
-    />
-    <div></div>
+  <main>
+    <section class="flex items-center gap-4 w-full">
+      <ActionBtn icon="<" label="Back" @click="onClickBack" />
+      <input
+        ref="noteTitle"
+        v-model="noteLabel"
+        type="text"
+        name="title"
+        :placeholder="noteLabel || 'New note'"
+        class="font-extralight text-3xl text-slate-500 w-full bg-transparent outline-none rounded-md"
+      />
+    </section>
 
-    <textarea
-      v-model="noteContent"
-      name="contents"
-      class="w-full h-full rounded-md outline-none text-slate-500 bg-transparent font-light resize-none"
-      :placeholder="noteContent || 'Jot something here...'"
-    ></textarea>
-    <div v-if="last_modified_by" class="text-right">
+    <section class="h-full mt-2">
+      <textarea
+        v-model="noteContent"
+        name="contents"
+        class="w-full h-full rounded-md outline-none text-slate-500 bg-transparent font-light resize-none"
+        :placeholder="noteContent || 'Jot something here...'"
+      ></textarea>
+    </section>
+
+    <section v-if="last_modified_by" class="text-right">
       <span class="text-xs font-thin italic text-slate-500">
         Updated by
         {{ last_modified_by }}
       </span>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts" setup>
 import { onUnmounted, ref, watch } from "vue"
-import { useRoute } from "vue-router"
-import HeaderSection from "../layouts/HeaderSection.vue"
+import { useRoute, useRouter } from "vue-router"
+// import HeaderSection from "../layouts/HeaderSection.vue"
 import { useNoteDetailsState, useNoteDetailsActions, clearNoteDetailsState } from "../store/noteDetails"
+import ActionBtn from "@/components/icons/ActionBtn.vue"
+import { useNotesListActions } from "@/store/notesList"
 
 const route = useRoute()
+const router = useRouter()
+
 const groupId = route.params.groupId as string
 
-const { note: noteFromStore } = useNoteDetailsState()
-const { updateNoteTitle, updateNoteContent, updateSharedNoteContent, updateSharedNoteTitle } = useNoteDetailsActions()
+const { note: noteFromStore, isEditing } = useNoteDetailsState()
+const { updateNoteTitle, updateNoteContent, updateSharedNoteContent, updateSharedNoteTitle, changeEditingState } = useNoteDetailsActions()
 
 const noteLabel = ref(noteFromStore.value?.title ? noteFromStore.value?.title : "")
 const noteContent = ref(noteFromStore.value?.contents ? noteFromStore.value?.contents : "")
 
 // @ts-ignore
 const last_modified_by = ref(noteFromStore.value?.last_modified_by)
+
+const { getPrivateNotesList, getSharedNotesList } = useNotesListActions()
+
+const onClickBack = async (): Promise<void> => {
+  if (isEditing.value) {
+    changeEditingState(false)
+    clearNoteDetailsState()
+    if (groupId) {
+      getSharedNotesList(groupId)
+    } else {
+      getPrivateNotesList()
+    }
+  }
+  router.back()
+}
 
 watch(
   () => noteLabel.value,
@@ -70,3 +93,16 @@ onUnmounted(() => {
   clearNoteDetailsState()
 })
 </script>
+
+<style scoped>
+main {
+  @apply h-screen p-3 flex flex-col justify-between;
+}
+textarea {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+textarea::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -1,16 +1,29 @@
 <template>
-  <MenuLayout back-btn="GroupsPage" header-label="shared notes" footer @close="getSearch">
-    <template #content="{ isScrolling }">
+  <main>
+    <section class="sticky top-0 px-3 pt-3 z-20 w-full duration-300 space-y-2" :class="!isPositionTop ? 'shadow-lg shadow-slate-500 rounded-b-2xl' : null">
+      <div class="flex items-center">
+        <h1 class="font-semibold">shared notes</h1>
+      </div>
+      <Transition name="searchSlide" mode="out-in">
+        <div v-if="isSearchActive">
+          <BaseInput v-model="search" placeholder="Search for note" class="nh-search drop-shadow-md" type="search" focus no-validate />
+        </div>
+      </Transition>
+    </section>
+
+    <section ref="el" class="h-full pt-2 pb-7 overflow-scroll px-3">
       <div v-if="isLoading" class="centered">
         <Spinner />
       </div>
+
       <div v-else-if="sharedNotesCount" class="pb-2">
         <MasonryWall :items="filteredNotes" :column-width="superSmall ? 150 : smallScreen ? 200 : 250" :gap="4">
           <template #default="{ item, index }">
-            <NoteCard :note="item" :color="item.color" :note-index="index" :is-scrolling="isScrolling" />
+            <NoteCard :note="item" :color="item.color" :note-index="index" />
           </template>
         </MasonryWall>
       </div>
+
       <div v-else class="centered">
         <div class="inline-flex flex-col justify-center items-center w-full">
           <p>You have no notes</p>
@@ -25,8 +38,13 @@
           </div>
         </div>
       </div>
-    </template>
-  </MenuLayout>
+    </section>
+
+    <section>
+      <FooterNavigator @on-click-search="toggleSearch" />
+      <SideNavigator />
+    </section>
+  </main>
 </template>
 
 <script lang="ts" setup>
@@ -34,11 +52,13 @@ import { useNotesListState, useNotesListGetters, useNotesListActions } from "@/s
 import Spinner from "@/components/base/Spinner.vue"
 import { useAppState } from "@/store/app"
 import CreateNoteIcon from "@/components/icons/CreateNoteIcon.vue"
-import MenuLayout from "@/layouts/MenuLayout.vue"
-import { computed, onMounted, ref, watch } from "vue"
+import { computed, onMounted, ref, watch, toRefs } from "vue"
 import { useRoute } from "vue-router"
-import { useBreakpoints } from "@vueuse/core"
+import { useBreakpoints, useScroll } from "@vueuse/core"
 import NoteCard from "@/components/notes/NoteCard.vue"
+import FooterNavigator from "@/components/navigators/FooterNavigator.vue"
+import SideNavigator from "@/components/navigators/SideNavigator.vue"
+import BaseInput from "@/components/base/BaseInput.vue"
 
 const breakpoints = useBreakpoints({
   xs: 436,
@@ -65,9 +85,15 @@ const filteredNotes = computed(() => {
     .reverse()
 })
 
-const getSearch = (value: string) => {
-  search.value = value
+const isSearchActive = ref(false)
+
+const toggleSearch = () => {
+  isSearchActive.value = !isSearchActive.value
 }
+
+const el = ref<HTMLElement | null>(null)
+const { arrivedState } = useScroll(el)
+const { top: isPositionTop } = toRefs(arrivedState)
 
 onMounted(() => {
   if (!sharedNotesCount.value) {
@@ -82,3 +108,23 @@ watch(
   },
 )
 </script>
+
+<style scoped>
+main {
+  @apply h-screen flex flex-col justify-between;
+}
+</style>
+
+<style>
+.searchSlide-leave-active {
+  transition: all 0.2s ease-out;
+}
+.searchSlide-enter-active {
+  transition: all 0.5s ease-in-out;
+}
+.searchSlide-enter-from,
+.searchSlide-leave-to {
+  opacity: 20%;
+  transform: translate(0, -10%);
+}
+</style>
