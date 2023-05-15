fe111da022d33a2ef13a2beb5056b5d893080c49
Ramzi <ramzi@dataddo.com>
2023-05-15 21:39:06 +0200

    Skeleton Loader

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,72 +1,20 @@
-<template>
-  <MasonryWall :items="data" :column-width="superSmall ? 150 : smallScreen ? 200 : 250" :gap="4" class="p-2">
-    <template #default="{ item, index }">
-      <div class="bg-yellow bg-yellow-400 rounded-xl" :class="item.height">{{ index }} {{ y }}</div>
-    </template>
-  </MasonryWall>
-</template>
-
 <script lang="ts" setup>
-import { useBreakpoints, useWindowScroll } from "@vueuse/core"
-
-const { y } = useWindowScroll()
-
-const breakpoints = useBreakpoints({
-  xs: 404,
-  sm: 608,
-})
-
-const superSmall = breakpoints.smaller("xs")
-const smallScreen = breakpoints.between("xs", "sm")
-
-const data = [
-  {
-    height: "h-52",
-  },
-  {
-    height: "h-44",
-  },
-  {
-    height: "h-48",
-  },
-  {
-    height: "h-60",
-  },
-  {
-    height: "h-44",
-  },
-  {
-    height: "h-52",
-  },
-  {
-    height: "h-44",
-  },
-  {
-    height: "h-60",
-  },
-  {
-    height: "h-52",
-  },
-  {
-    height: "h-44",
-  },
-  {
-    height: "h-48",
-  },
-  {
-    height: "h-60",
-  },
-  {
-    height: "h-44",
-  },
-  {
-    height: "h-52",
-  },
-  {
-    height: "h-44",
-  },
-  {
-    height: "h-60",
-  },
-]
+import SkeletonLoader from "./Skeleton.vue"
 </script>
+
+<template>
+  <div class="p-4 max-w-sm rounded border border-gray-200 md:p-6">
+    <SkeletonLoader class="mb-4 h-48 flex justify-center items-center text-gray-400"> Image </SkeletonLoader>
+    <SkeletonLoader class="h-2.5 w-48 mb-4" />
+    <SkeletonLoader class="h-2 mb-2.5" />
+    <SkeletonLoader class="h-2 mb-2.5" />
+    <SkeletonLoader class="h-2" />
+    <div class="flex items-center mt-4 space-x-3">
+      <SkeletonLoader type="circle" class="w-14 h-14 flex justify-center items-center text-gray-400"> Icon </SkeletonLoader>
+      <div>
+        <SkeletonLoader class="h-2.5 w-32 mb-2"></SkeletonLoader>
+        <SkeletonLoader class="w-48 h-2" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/router/router.ts
+++ src/router/router.ts
@@ -15,14 +15,14 @@ export const routes = [
     },
   },
 
-  // {
-  //   path: "/test",
-  //   name: "TestPage",
-  //   component: () => import("../pages/TestPage.vue"),
-  //   meta: {
-  //     requiresAuth: true,
-  //   },
-  // },
+  {
+    path: "/test",
+    name: "TestPage",
+    component: () => import("../pages/TestPage.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
   {
     path: "/groups",
     name: "GroupsPage",
