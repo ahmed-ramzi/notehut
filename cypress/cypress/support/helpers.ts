5a447283d5470623e375e409ea6263a3f9a490ee
Ramzi <ramzi@dataddo.com>
2023-05-21 18:52:01 +0200

    Temporary Removing Cypress

--- cypress/cypress/support/helpers.ts
+++
@@ -1,8 +0,0 @@
-export default class Helpers {
-  urlContains(value: string) {
-    cy.url().should("include", value)
-  }
-  urlDoesNotContains(value: string) {
-    cy.url().should("not.include", value)
-  }
-}
